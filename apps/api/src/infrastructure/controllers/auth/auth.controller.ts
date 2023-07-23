5345faa6cc838da6e9574f9865198f88dc5857d3
Thibaut Pontieux <thibaut.pontieux@gmail.com>
2023-07-23 20:16:29 +0200

    add token validity status route

--- apps/api/src/infrastructure/controllers/auth/auth.controller.ts
+++ apps/api/src/infrastructure/controllers/auth/auth.controller.ts
@@ -160,10 +160,15 @@ export class AuthController {
   @Post('change-password/:token')
   @HttpCode(200)
   @ApiOperation({ description: 'change the password of an account' })
-  async changePasswordWithToken(@Param('token') token: string, @Body() accountPassword: AuthPasswordDto) {
-    const response = await this.resetPasswordUsecaseProxy.getInstance().resetPassword(token, accountPassword.password);
-    
-    return JsonResult.Convert('The password has been changed');
+  async changePasswordWithToken(
+    @Param('token') token: string,
+    @Body() accountPassword: AuthPasswordDto,
+  ) {
+    const response = await this.resetPasswordUsecaseProxy
+      .getInstance()
+      .resetPassword(token, accountPassword.password);
+
+    return JsonResult.Convert('the password has been changed');
   }
 
   @Get('is-valid/:token')
@@ -173,5 +178,6 @@ export class AuthController {
     const response = await this.resetPasswordUsecaseProxy.getInstance().tokenIsValid(token);
     
     return response.statusCode ? response : JsonResult.Convert('The token is valid');
+
   }
 }
