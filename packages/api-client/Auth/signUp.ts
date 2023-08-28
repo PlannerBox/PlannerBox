a5202a443c9a785a679505ba542ce1512cd06bfd
Rudy B <btte.rudy@gmail.com>
2023-08-28 14:47:11 +0200

    Impelment SignUp API

---
+++ packages/api-client/Auth/signUp.ts
@@ -0,0 +1,24 @@
+import { apiCall } from '../utils/api';
+
+export type SignUpProps = {
+  firstname: string;
+  lastname: string;
+  password: string;
+  birthDate: Date;
+  birthPlace: string;
+  role: string;
+  formationMode: string;
+};
+
+export type SignUpResponse = {};
+
+const signUp = async ({
+  ...userData
+}: SignUpProps): Promise<SignUpResponse> => {
+  return await apiCall(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/signup`, {
+    method: 'POST',
+    body: JSON.stringify({ ...userData }),
+  });
+};
+
+export { signUp };
