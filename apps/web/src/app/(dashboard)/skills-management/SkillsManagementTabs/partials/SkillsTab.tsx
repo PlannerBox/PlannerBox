a802182a100c6ba8342a30c0516ee8d11edcab74
Rudy Boutte <32516879+boutterudy@users.noreply.github.com>
2023-09-06 19:26:28 +0200

    Implement Edit Popover Form (#28)

--- apps/web/src/app/(dashboard)/skills-management/SkillsManagementTabs/partials/SkillsTab.tsx
+++ apps/web/src/app/(dashboard)/skills-management/SkillsManagementTabs/partials/SkillsTab.tsx
@@ -8,69 +8,20 @@ type SkillsTabProps = {
   step?: 'list';
 };
 
-interface SkillsDataType {
-  key: string;
-  name: string;
-  internal_teachers: number;
-  external_teachers: number;
-}
-
-const skillsColumns: ColumnsType<SkillsDataType> = [
-  {
-    title: 'Nom',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Formateurs internes',
-    dataIndex: 'internal_teachers',
-    key: 'internal_teachers',
-  },
-  {
-    title: 'Formateurs externes',
-    dataIndex: 'external_teachers',
-    key: 'external_teachers',
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: () => (
-      <Space size='middle'>
-        <EditOutlined />
-        <MoreOutlined />
-      </Space>
-    ),
-  },
-];
-
-const skillsData: SkillsDataType[] = [
-  {
-    key: '1',
-    name: 'Big Data',
-    internal_teachers: 0,
-    external_teachers: 3,
-  },
-  {
-    key: '2',
-    name: 'Big Data',
-    internal_teachers: 42,
-    external_teachers: 12,
-  },
-  {
-    key: '3',
-    name: 'Big Data',
-    internal_teachers: 32,
-    external_teachers: 99,
-  },
-];
-
-type SkillFieldType = {
-  name?: string;
-};
-
 export default function SkillsTab({ step = 'list' }: SkillsTabProps) {
-  const [form] = Form.useForm<SkillFieldType>();
-  const skillNameValue = Form.useWatch('name', form);
+  interface SkillsDataType {
+    key: string;
+    name: string;
+    internal_teachers: number;
+    external_teachers: number;
+  }
+
+  type SkillFieldType = {
+    name?: string;
+  };
+
+  const [creationForm] = Form.useForm<SkillFieldType>();
+  const skillNameCreationValue = Form.useWatch('name', creationForm);
 
   const onSkillCreationFinish = (values: any) => {
     console.log('Success:', values);
@@ -81,7 +32,7 @@ export default function SkillsTab({ step = 'list' }: SkillsTabProps) {
   };
 
   const onSkillCreationReset = () => {
-    form.resetFields();
+    creationForm.resetFields();
   };
 
   const addSkillPopoverContent = () => (
@@ -91,7 +42,7 @@ export default function SkillsTab({ step = 'list' }: SkillsTabProps) {
       onFinish={onSkillCreationFinish}
       onFinishFailed={onSkillCreationFinishFailed}
       autoComplete='off'
-      form={form}
+      form={creationForm}
       style={{
         display: 'flex',
         flexDirection: 'column',
@@ -106,13 +57,115 @@ export default function SkillsTab({ step = 'list' }: SkillsTabProps) {
         <Button
           type='primary'
           htmlType='submit'
-          disabled={(skillNameValue?.length || 0) <= 0}
+          disabled={(skillNameCreationValue?.length || 0) <= 0}
         >
           Créer
         </Button>
       </Form.Item>
     </Form>
   );
+
+  const [editForm] = Form.useForm<SkillFieldType>();
+  const skillNameEditValue = Form.useWatch('name', editForm);
+
+  const onSkillEditFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onSkillEditFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const onSkillEditReset = () => {
+    editForm.resetFields();
+  };
+
+  const editSkillPopoverContent = (defaultValue: string) => (
+    <Form
+      name='basic'
+      initialValues={{}}
+      onFinish={onSkillEditFinish}
+      onFinishFailed={onSkillEditFinishFailed}
+      autoComplete='off'
+      form={editForm}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 'var(--spacing-12)',
+      }}
+    >
+      <Form.Item<SkillFieldType> name='name' style={{ margin: 0 }}>
+        <Input defaultValue={defaultValue} />
+      </Form.Item>
+
+      <Form.Item style={{ margin: 0 }}>
+        <Button
+          type='primary'
+          htmlType='submit'
+          disabled={(skillNameEditValue?.length || 0) <= 0}
+        >
+          Modifier
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+
+  const skillsColumns: ColumnsType<SkillsDataType> = [
+    {
+      title: 'Nom',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Formateurs internes',
+      dataIndex: 'internal_teachers',
+      key: 'internal_teachers',
+    },
+    {
+      title: 'Formateurs externes',
+      dataIndex: 'external_teachers',
+      key: 'external_teachers',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space size='middle'>
+          <Popover
+            placement='leftTop'
+            title='Nom de la compétence'
+            content={() => editSkillPopoverContent(record.name)}
+            trigger='click'
+            onOpenChange={onSkillEditReset}
+          >
+            <EditOutlined />
+          </Popover>
+          <MoreOutlined />
+        </Space>
+      ),
+    },
+  ];
+
+  const skillsData: SkillsDataType[] = [
+    {
+      key: '1',
+      name: 'Big Data',
+      internal_teachers: 0,
+      external_teachers: 3,
+    },
+    {
+      key: '2',
+      name: 'Big Data',
+      internal_teachers: 42,
+      external_teachers: 12,
+    },
+    {
+      key: '3',
+      name: 'Big Data',
+      internal_teachers: 32,
+      external_teachers: 99,
+    },
+  ];
 
   return (
     <div>
