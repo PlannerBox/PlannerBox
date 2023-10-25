0da54f7a514d1c41a520ed81e7c855ddf85c7b42
thibaut <thibaut.pontieux2020@campus-eni.fr>
2023-10-25 21:41:58 +0200

    try something wierd

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,4 @@
+const { cookies } = require('next/dist/client/components/headers');
 const path = require('path');
 
 module.exports = {
@@ -6,5 +7,12 @@ module.exports = {
   output: 'standalone',
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
+  async headers() {
+    var cookies = cookies()
+    return [{
+      key: 'Cookie',
+      value: `session=${cookies.get('session')?.value}; HttpOnly; Path=/; Max-Age=${process.env.JWT_EXPIRATION_TIME}`,
+    }]
   }
 };
